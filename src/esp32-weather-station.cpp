80e8baebe4dfe75c6c9188fb5ddbb1842e670c59
schmallaria <Christian.Schmalz@comparex.com>
2018-07-24 13:50:55 +0200

    x

---
+++ src/esp32-weather-station.cpp
@@ -0,0 +1,22 @@
+/***
+ * Install the following libraries
+ * - Mini Grafx by Daniel Eichhorn
+ * - DallasTemperature by Guil Barros
+ ***/
+#include <Arduino.h>
+
+#include "settings.h"
+
+/*temperature lib*/
+#include "OneWire.h"
+#include "DallasTemperature.h"
+#include <SPI.h>
+#include "MiniGrafx.h" // General graphic library
+#include "ILI9341_SPI.h" // Hardware-specific library
+
+void setup() {
+    
+}
+
+void loop() {
+}
